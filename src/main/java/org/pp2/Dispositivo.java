f730a615d479f1bf0d3b2589ebcd87526c6de4e3
lucas <lucas.addisi@gmail.com>
2023-10-01 19:46:12 -0300

    Se agrega constructor sin parámetros a Dispositivo

--- src/main/java/org/pp2/Dispositivo.java
+++ src/main/java/org/pp2/Dispositivo.java
@@ -3,8 +3,10 @@ package org.pp2;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor // Necesario para poder serializar/deserializar.
 @AllArgsConstructor
 public abstract class Dispositivo {
 
